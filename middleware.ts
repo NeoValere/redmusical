17b29cba0015065882a4a487e89e65bf637b1ce6
NeoValere <leandro@valererealms.com>
2025-06-25 09:33:28 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,15 @@
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|images|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+  runtime: 'nodejs',
+};
+
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+
+
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
@@ -55,16 +64,3 @@ export async function middleware(req: NextRequest) {
   return res;
 }
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder (e.g. /images)
-     * - Any other static assets or public routes that don't need authentication
-     */
-    '/((?!_next/static|_next/image|favicon.ico|images|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
-};

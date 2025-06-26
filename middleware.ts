53f7dda0e9592dd592d7a16c20eb2aa5697c60c4
NeoValere <leandro@valererealms.com>
2025-06-26 01:05:42 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@/utils/supabase/middleware'
+import { createClient } from './utils/supabase/middleware'
 import { type NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
